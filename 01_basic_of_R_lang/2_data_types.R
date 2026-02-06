95ea2c047a46c6e1032e6f59df72f1e357b4ae65
sanjana012s <sanjana081220@gmail.com>
2026-02-06 18:19:25 +0530

    Initial commit

---
+++ 01_basic_of_R_lang/2_data_types.R
@@ -0,0 +1,28 @@
+#Numeric data type
+age <- 20
+price <- 20.9
+cat(age)
+cat(price)
+cat(price+age)
+class(age)
+
+
+#character(String data type)
+
+name <- "sanjna"
+cat(name)
+class(name)
+
+
+#logical data type
+student <- TRUE
+pass <- FALSE
+cat(student)
+cat(pass)
+class(student)
+
+
+
+
+
+
---
+++ 01_basic_of_R_lang/3_operator.R
@@ -0,0 +1,24 @@
+#Arithmetic Operators
+a <- 10
+b <- 12
+cat(a+b)
+cat(a-b)
+cat(a*b)
+cat(a/b)
+
+
+#comparison operator
+a>b
+a>=b
+a<b
+a<=b
+a==b
+a!=b
+
+#Logical Operators (Conditions)
+x <- TRUE
+y <- FALSE
+x&&y
+x|y
+!x
+
